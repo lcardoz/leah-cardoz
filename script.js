36edde751322d5ccba28217e63032e7d21a8028c
Leah Cardoz <leahcardoz@gmail.com>
2023-01-02 13:21:34 -0500

    changed title, bio description

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 // h2.textContent = "A BIT ABOUT ME:";
 
 // document.querySelector("script").appendChild(h2);
+
+const imgLeahSection = document.querySelector('#leah-photo');
+
+imgLeahSection.addEventListener('click', (e) => {
+	const imgLeah = e.target;
+	imgLeah.src =
+		'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2021%2F04%2Ffunimation-how-to-make-naruto-ramen-recipe-000.jpg?w=960&cbr=1&q=90&fit=max';
+});
